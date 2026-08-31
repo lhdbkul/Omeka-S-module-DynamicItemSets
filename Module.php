747cb1ca115c5c58ff00d2a6d6882c56a2707436
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-08-31 00:00:00 +0000

    Released version 3.4.7.

--- Module.php
+++ Module.php
@@ -54,12 +54,11 @@ class Module extends AbstractModule
     {
         $services = $this->getServiceLocator();
         $plugins = $services->get('ControllerPluginManager');
-        $translate = $plugins->get('translate');
         $translator = $services->get('MvcTranslator');
 
         if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.91')) {
             $message = new \Omeka\Stdlib\Message(
-                $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
+                $translator->translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
                 'Common', '3.4.91'
             );
             throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
@@ -335,7 +334,7 @@ class Module extends AbstractModule
                 <h4>$title</h4>
                 <div class="value">$value</div>
             </div>
-            
+
             HTML;
     }
 
